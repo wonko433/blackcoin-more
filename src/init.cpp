3eeeb079bd37bafe391145c1727099d8056fab24
MarcoFalke <falke.marco@gmail.com>
2016-01-08 01:14:09 +0100

    [wallet] Add high transaction fee warnings

--- src/init.cpp
+++ src/init.cpp
@@ -960,10 +960,10 @@ bool AppInit2(Config& config, boost::thread_group& threadGroup, CScheduler& sche
     if (mapArgs.count("-minrelaytxfee"))
     {
         CAmount n = 0;
-        if (ParseMoney(mapArgs["-minrelaytxfee"], n) && n > 0)
-            ::minRelayTxFee = CFeeRate(n);
-        else
+        if (!ParseMoney(mapArgs["-minrelaytxfee"], n))
             return InitError(AmountErrMsg("minrelaytxfee", mapArgs["-minrelaytxfee"]));
+        // High fee check is done afterward in CWallet::ParameterInteraction()
+        ::minRelayTxFee = CFeeRate(n);
     }
 
     fRequireStandard = !GetBoolArg("-acceptnonstdtxn", !Params().RequireStandard());
