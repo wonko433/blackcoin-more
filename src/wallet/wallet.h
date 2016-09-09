cda1bae86a7718a7996eea44fe89119246db6cae
Luke Dashjr <luke-jr+git@utopios.org>
2016-09-09 08:44:47 +0000

    Wallet: Split main logic from InitLoadWallet into CreateWalletFromFile

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -975,6 +975,7 @@ public:
     static std::string GetWalletHelpString(bool showDebug);
 
     /* Initializes the wallet, returns a new CWallet instance or a null pointer in case of an error */
+    static CWallet* CreateWalletFromFile(const std::string walletFile);
     static bool InitLoadWallet();
 
     /**
