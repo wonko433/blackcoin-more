27e896ff73f488602a80731e2bc3d1a4b72fa0a2
Cory Fields <cory-nospam-@coryfields.com>
2016-12-31 02:05:30 -0500

    net: add a flag to indicate when a node's process queue is full
    Messages are dumped very quickly from the socket handler to the processor, so
    it's the depth of the processing queue that's interesting.
    
    The socket handler checks the process queue's size during the brief message
    hand-off and pauses if necessary, and the processor possibly unpauses each time
    a message is popped off of its queue.

--- src/net.h
+++ src/net.h
@@ -612,6 +612,7 @@ public:
 
     CCriticalSection cs_vProcessMsg;
     std::list<CNetMessage> vProcessMsg;
+    size_t nProcessQueueSize;
 
     std::deque<CInv> vRecvGetData;
     std::list<CNetMessage> vRecvMsg;
@@ -653,6 +654,7 @@ public:
     const NodeId id;
 
     const uint64_t nKeyedNetGroup;
+    std::atomic_bool fPauseRecv;
 protected:
 
     mapMsgCmdSize mapSendBytesPerMsgCmd;
@@ -744,15 +746,6 @@ public:
     {
         assert(nRefCount >= 0);
         return nRefCount;
-    }
-
-    // requires LOCK(cs_vRecvMsg)
-    unsigned int GetTotalRecvSize()
-    {
-        unsigned int total = 0;
-        BOOST_FOREACH(const CNetMessage &msg, vRecvMsg)
-            total += msg.vRecv.size() + 24;
-        return total;
     }
 
     // requires LOCK(cs_vRecvMsg)
