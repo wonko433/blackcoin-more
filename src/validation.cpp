1da3e60a91a731cf00099876e4953a7282711401
Alex Morcos <morcos@chaincode.com>
2017-01-19 23:45:02 -0500

    [bugfix] save feeDelta instead of priorityDelta in DumpMempool

--- src/validation.cpp
+++ src/validation.cpp
@@ -4345,7 +4345,7 @@ void DumpMempool(void)
     {
         LOCK(mempool.cs);
         for (const auto &i : mempool.mapDeltas) {
-            mapDeltas[i.first] = i.second.first;
+            mapDeltas[i.first] = i.second.second;
         }
         vinfo = mempool.infoAll();
     }
