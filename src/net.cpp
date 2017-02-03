1962ffe6829a6864bc5c0fb2baef82bf81eb0b7e
Matt Corallo <git@bluematt.me>
2017-02-02 20:03:46 -0500

    Lock cs_vSend and cs_inventory in a consistent order even in TRY

--- src/net.cpp
+++ src/net.cpp
@@ -1146,12 +1146,13 @@ void CConnman::ThreadSocketHandler()
                 {
                     bool fDelete = false;
                     {
-                        TRY_LOCK(pnode->cs_vSend, lockSend);
-                        if (lockSend)
+                        TRY_LOCK(pnode->cs_inventory, lockInv);
+                        if (lockInv)
                         {
-                                TRY_LOCK(pnode->cs_inventory, lockInv);
-                                if (lockInv)
-                                    fDelete = true;
+                            TRY_LOCK(pnode->cs_vSend, lockSend);
+                            if (lockSend) {
+                                fDelete = true;
+                            }
                         }
                     }
                     if (fDelete)
