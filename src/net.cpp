d544b1377eb3f7d28123ac119238b10b243b2e47
Matt Corallo <git@bluematt.me>
2017-02-06 11:44:38 -0500

    Avoid copying CNodeStats to make helgrind OK with buggy std::string

--- src/net.cpp
+++ src/net.cpp
@@ -2424,9 +2424,8 @@ void CConnman::GetNodeStats(std::vector<CNodeStats>& vstats)
     vstats.reserve(vNodes.size());
     for(std::vector<CNode*>::iterator it = vNodes.begin(); it != vNodes.end(); ++it) {
         CNode* pnode = *it;
-        CNodeStats stats;
-        pnode->copyStats(stats);
-        vstats.push_back(stats);
+        vstats.emplace_back();
+        pnode->copyStats(vstats.back());
     }
 }
 
