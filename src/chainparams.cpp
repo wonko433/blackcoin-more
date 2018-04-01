a5646b4446b15f97942cd1c2c152ce9a9a39e667
lateminer <late-miner@ya.ru>
2018-04-01 21:43:48 +0300

    Add one more hardcoded node

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -151,6 +151,7 @@ public:
         assert(genesis.hashMerkleRoot == uint256S("0x0e58d4b3215bb1889d5a027d40269a167b93c68e9ae28961de4717558be92d38"));
 
         vSeeds.push_back(CDNSSeedData("dopecoin.com", "dnsseed.dopecoin.com"));
+        vSeeds.push_back(CDNSSeedData("188.166.89.189", "188.166.89.189"));
         vSeeds.push_back(CDNSSeedData("37.120.190.76", "37.120.190.76"));
         vSeeds.push_back(CDNSSeedData("37.120.186.85", "37.120.186.85"));
         vSeeds.push_back(CDNSSeedData("188.68.52.172", "188.68.52.172"));
