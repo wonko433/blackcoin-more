tmagik/catoshi

rpc: Argument name consistency
The meaning is clear from the context, and we're inconsistent here.
Also save typing when using named arguments.

- `bitcoinaddress` -> `address`
- `bitcoinprivkey` -> `privkey`
- `bitcoinpubkey` -> `pubkey`

src/rpc/blockchain.cpp
@@ -875,7 +875,7 @@ UniValue gettxout(const JSONRPCRequest& request)
             "     \"reqSigs\" : n,          (numeric) Number of required signatures\n"
             "     \"type\" : \"pubkeyhash\", (string) The type, eg pubkeyhash\n"
             "     \"addresses\" : [          (array of string) array of bitcoin addresses\n"
-            "        \"bitcoinaddress\"     (string) bitcoin address\n"
+            "        \"address\"     (string) bitcoin address\n"
             "        ,...\n"
             "     ]\n"
             "  },\n"

src/wallet/rpcdump.cpp
@@ -520,11 +520,11 @@ UniValue dumpprivkey(const JSONRPCRequest& request)
     
     if (request.fHelp || request.params.size() != 1)
         throw runtime_error(
-            "dumpprivkey \"bitcoinaddress\"\n"
-            "\nReveals the private key corresponding to 'bitcoinaddress'.\n"
+            "dumpprivkey \"address\"\n"
+            "\nReveals the private key corresponding to 'address'.\n"
             "Then the importprivkey can be used with this output\n"
             "\nArguments:\n"
-            "1. \"bitcoinaddress\"   (string, required) The bitcoin address for the private key\n"
+            "1. \"address\"   (string, required) The bitcoin address for the private key\n"
             "\nResult:\n"
             "\"key\"                (string) The private key\n"
             "\nExamples:\n"

wonko433/blackcoin-more

src/rpc/blockchain.cpp
@@ -921,7 +921,7 @@ UniValue gettxout(const JSONRPCRequest& request)
             "     \"reqSigs\" : n,          (numeric) Number of required signatures\n"
             "     \"type\" : \"pubkeyhash\", (string) The type, eg pubkeyhash\n"
             "     \"addresses\" : [          (array of string) array of blackcoin addresses\n"
-            "        \"blackcoinaddress\"     (string) blackcoin address\n"
+            "        \"address\"     (string) blackcoin address\n"
             "        ,...\n"
             "     ]\n"
             "  },\n"

src/wallet/rpcdump.cpp
@@ -545,11 +545,11 @@ UniValue dumpprivkey(const JSONRPCRequest& request)
     
     if (request.fHelp || request.params.size() != 1)
         throw runtime_error(
-            "dumpprivkey \"blackcoinaddress\"\n"
-            "\nReveals the private key corresponding to 'blackcoinaddress'.\n"
+            "dumpprivkey \"address\"\n"
+            "\nReveals the private key corresponding to 'address'.\n"
             "Then the importprivkey can be used with this output\n"
             "\nArguments:\n"
-            "1. \"blackcoinaddress\"   (string, required) The blackcoin address for the private key\n"
+            "1. \"address\"   (string, required) The blackcoin address for the private key\n"
             "\nResult:\n"
             "\"key\"                (string) The private key\n"
             "\nExamples:\n"