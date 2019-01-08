14b8310e873e3778523729832fcc64be5b6b103c
lateminer <late-miner@ya.ru>
2019-01-08 23:14:37 +0300

    Ignore block sigs while importing headers

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2164,6 +2164,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         for (unsigned int n = 0; n < nCount; n++) {
             vRecv >> headers[n];
             ReadCompactSize(vRecv); // ignore tx count; assume it is 0.
+            ReadCompactSize(vRecv); // ignore block sig; assume it is 0.
         }
 
         if (nCount == 0) {
