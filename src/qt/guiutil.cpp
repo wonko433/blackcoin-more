fef5b5542d3a454b55d5c59d0b950274137d60c7
Michel van Kessel <michelvankessel@protonmail.com>
2020-09-19 11:05:08 +0200

    Bump the minimum Qt version to 5.2

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -87,13 +87,7 @@ QString dateTimeStr(qint64 nTime)
 
 QFont fixedPitchFont()
 {
-#if QT_VERSION >= 0x50200
     return QFontDatabase::systemFont(QFontDatabase::FixedFont);
-#else
-    QFont font("Monospace");
-    font.setStyleHint(QFont::Monospace);
-    return font;
-#endif
 }
 
 // Just some dummy data to generate a convincing random-looking (but consistent) address
