b23619aecd51a74eee3ca62b71ea1dc277a1418d
lateminer <9951982+lateminer@users.noreply.github.com>
2020-09-24 21:45:08 +0300

    Comment out PoW check in LoadBlockIndexGuts()

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -275,10 +275,18 @@ bool CBlockTreeDB::LoadBlockIndexGuts(const Consensus::Params& consensusParams, 
                 pindexNew->nStakeModifier = diskindex.nStakeModifier;
                 pindexNew->nTx            = diskindex.nTx;
 
+                // Litecoin: Disable PoW Sanity check while loading block index from disk.
+                // We use the sha256 hash for the block index for performance reasons, which is recorded for later use.
+                // CheckProofOfWork() uses the scrypt hash which is discarded after a block is accepted.
+                // While it is technically feasible to verify the PoW, doing so takes several minutes as it
+                // requires recomputing every PoW hash during every Litecoin startup.
+                // We opt instead to simply trust the data that is on your local disk.
+				/*
                 if (pindexNew->IsProofOfWork()) {
                     if (!CheckProofOfWork(pindexNew->GetBlockPoWHash(), pindexNew->nBits, consensusParams))
                         return error("%s: CheckProofOfWork failed: %s", __func__, pindexNew->ToString());
                 }
+				*/
 
                 pcursor->Next();
             } else {
