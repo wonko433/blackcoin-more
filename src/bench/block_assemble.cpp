f9bb8dc9638f67cea098697fc84deff66061fca9
lateminer <9951982+lateminer@users.noreply.github.com>
2020-09-24 20:19:39 +0300

    Fix bench/block_assemble.cpp

--- src/bench/block_assemble.cpp
+++ src/bench/block_assemble.cpp
@@ -53,7 +53,13 @@ static CTxIn MineBlock(const CScript& coinbase_scriptPubKey)
 
 static void AssembleBlock(benchmark::State& state)
 {
-    const std::vector<unsigned char> op_true{OP_TRUE};
+    const CScript redeemScript = CScript() << OP_DROP << OP_TRUE;
+    const CScript SCRIPT_PUB =
+        CScript() << OP_HASH160 << ToByteVector(CScriptID(redeemScript))
+                  << OP_EQUAL;
+
+    const CScript scriptSig = CScript() << std::vector<uint8_t>(100, 0xff)
+                                        << ToByteVector(redeemScript);
 
     // Switch to regtest so we can mine faster
     SelectParams(CBaseChainParams::REGTEST);
@@ -77,11 +83,13 @@ static void AssembleBlock(benchmark::State& state)
     }
 
     // Collect some loose transactions that spend the coinbases of our mined blocks
-    constexpr size_t NUM_BLOCKS{200};
+    constexpr size_t NUM_BLOCKS{600};
+    constexpr int COINBASE_MATURITY = 500;
     std::array<CTransactionRef, NUM_BLOCKS - COINBASE_MATURITY + 1> txs;
     for (size_t b{0}; b < NUM_BLOCKS; ++b) {
         CMutableTransaction tx;
         tx.vin.push_back(MineBlock(SCRIPT_PUB));
+        tx.vin.back().scriptSig = scriptSig;
         tx.vout.emplace_back(1337, SCRIPT_PUB);
         if (NUM_BLOCKS - b >= COINBASE_MATURITY)
             txs.at(b) = MakeTransactionRef(tx);
